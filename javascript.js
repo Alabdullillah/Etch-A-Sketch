4376f6c80cc3cb830b7411ed4040525547e4cba9
Alabdulillah <spiderspideremo@gmail.com>
2024-06-28 11:24:54 +0300

    creating a container and a grid excelusivly via JavaScript.

---
+++ javascript.js
@@ -0,0 +1,11 @@
+var container = document.createElement("div");
+var grid = document.createElement("div");
+container.setAttribute("id","container")
+
+//document.body.appendChild(container);
+//document.container.appendChild(grid);
+
+addEventListener('load', (event) => {
+    document.body.appendChild(container);
+    //document.container.appendChild(grid);
+})
