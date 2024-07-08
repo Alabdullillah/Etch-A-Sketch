0054a7093784c94ef45f8b928ba81ffdb272a11d
Alabdulillah <spiderspideremo@gmail.com>
2024-07-08 09:39:46 +0300

    animated grid when size changes _ the slider numbers change with user input

--- javascript.js
+++ javascript.js
@@ -6,45 +6,57 @@ let eraser = document.getElementById("eraser")
 let size = document.querySelector("#size");
 let sliderValue = document.querySelector("#sliderValue") 
 
-
-let g = Math.round((Math.random() * 255))
-let b = Math.round((Math.random() * 255))
-let r = Math.round((Math.random() * 255))
-
 sliderValue.textContent = `${size.value} * ${size.value}`
 //document.body.appendChild(container);
 //document.container.appendChild(grid);
 
 function createGrid(size) {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild)
+    }   
  
     for (let i = 0; i < size * size; i++){
         let griditem = document.createElement("div");
-       // griditem.style.border = "0.1px solid black";
+        griditem.style.border = "1px solid black";
         griditem.style.background = "white";
         griditem.setAttribute("class", "cells");
-        griditem.style.width = `${600/size}px`;
-        griditem.style.height = `${600/size}px`;
+        griditem.style.width = `${(600/size) - 2}px`;
+        griditem.style.height = `${(600/size) - 2}px`;
+        let g = Math.round((Math.random() * 255))
+        let b = Math.round((Math.random() * 255))
+        let r = Math.round((Math.random() * 255))
         
         container.appendChild(griditem);
         
         griditem.addEventListener("mouseover", () => {
             griditem.style.backgroundColor = "black";
-        })
+       })
 
-        griditem.addEventListener("mouseout", () => {
-            griditem.style.backgroundColor = `rgb(${r},${g},${b})`;
-        })
-        
+     griditem.addEventListener("mouseout", () => {
+       griditem.style.backgroundColor = `rgb(${r},${g},${b})`;
+     })
+        //rgb (2,2,3)
     }
 }
 
+size.addEventListener("input", () => {
+    sliderValue.textContent = `${size.value} * ${size.value}`
+    createGrid(size.value);
+})
+
 button.addEventListener("click", (event) => {
     //container.appendChild(grid);
-    while (container.firstChild) {
-        container.removeChild(container.firstChild)
-    }
-    createGrid(size.value);
-    sliderValue.textContent = `${size.value} * ${size.value}`
+   // while (container.firstChild) {
+   //     container.removeChild(container.firstChild)
+// }
+//        let cell = Document.getElementsByClassName("cells") ;
+//           cell.addEventListener("mouseover", () => {
+//            cell.style.backgroundColor = "black";
+//        })
+//           cell.addEventListener("mouseout", () => {
+//            cell.style.backgroundColor = `rgb(${r},${g},${b})`;
+//        })
+    
 })
 
 eraser.addEventListener("click", (event) => {
